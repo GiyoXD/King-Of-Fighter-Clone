7da875aa6be61d3044620e5949b452167e031c36
GiyoXD <menchayheng@DESKTOP-2VRONI2>
2024-08-26 18:54:22 +0700

    first verision

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")    
