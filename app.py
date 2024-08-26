309867d2df8c6b7aeebcab4ebcd3efd03e79d1b8
GiyoXD <menchayheng@DESKTOP-2VRONI2>
2024-08-26 19:06:08 +0700

    working version is here

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    if request.method == "GET":
-        return render_template("index.html")    
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
